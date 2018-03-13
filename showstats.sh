2b0441d1513cfd0412e3264bb81b58416a88363b
Tsugikazu SHIBATA <tsugkazu.shibata@gmail.com>
2018-03-13 23:53:04 +0900

    More fix for GITREPO and GITDIR

--- showstats.sh
+++ showstats.sh
@@ -21,8 +21,7 @@ echo -e "GITREPO: " $GITREPO
 echo -ne "Date: "; date
 echo -e "Version" " " "Date\t\t#cmiter\t#cmits\t#domain"
 # start version report
-dir=$GITREPO
-cd $dir
+cd $GITDIR
 if [ "$VERSION_TAG_UNFILTER" != "" ]; then
     ver=`git tag -l  |grep $VERSION_TAG_FILTER| grep -v $VERSION_TAG_UNFILTER|sort -r --version-sort `
 else
@@ -32,7 +31,7 @@ for i in  $ver
 do
   echo  -n $i " "
   set `git log -1 --pretty=format:%ai $i`; echo -ne $1 '\t' #print tag and date
-  git shortlog -sne $i| domcount.py ; echo -n
+  git shortlog -sne $i| domcount ; echo -n
   #set `git shortlog -s $i |wc -l`; echo -e $1 '\t'  #  print num contributors
   #(cd ..;  sh gitlinecount.sh $dir $i)
 done 
